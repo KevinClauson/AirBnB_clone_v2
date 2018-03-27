bc43dec9a660e5b3fdafb35f0ae9c0209e03ea7d
KevinClauson <kevinclauson99@gmail.com>
2018-03-27 20:29:30 +0000

    add city class

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 '''
     Define the class City.
 '''
+from os import environ
 from models.base_model import BaseModel
 
 
@@ -9,5 +10,10 @@ class City(BaseModel):
     '''
         Define the class City that inherits from BaseModel.
     '''
-    state_id = ""
-    name = ""
+    if environ.get('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'))
+        name = Column(String(128), nullable=False)
+    else:
+        state_id = ""
+        name = ""
