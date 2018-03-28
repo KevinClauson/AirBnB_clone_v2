9bd5e561d96eb109e74d0499a33d1562e13cea2d
KevinClauson <kevinclauson99@gmail.com>
2018-03-28 02:59:10 +0000

    add user.py class

--- models/user.py
+++ models/user.py
@@ -2,14 +2,23 @@
 '''
     Implementation of the User class which inherits from BaseModel
 '''
-from models.base_model import BaseModel
+from os import environ
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
-
-class User(BaseModel):
+class User(BaseModel, Base):
     '''
         Definition of the User class
     '''
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    if environ.get('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
