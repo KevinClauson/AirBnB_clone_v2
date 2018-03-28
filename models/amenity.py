721baf2c900f6ad188bf3fbe52707a65844fa034
Dan Parham <dparha012@gmail.com>
2018-03-28 21:14:02 +0000

    moved import of table instance place_amenity

--- models/amenity.py
+++ models/amenity.py
@@ -7,7 +7,6 @@ from models.base_model import Base
 from os import environ
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
@@ -15,6 +14,7 @@ class Amenity(BaseModel, Base):
         Implementation for the Amenities.
     '''
     if environ.get("HBNB_TYPE_STORAGE") == "db":
+        from models.place import place_amenity
         __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
         place_amenities = relationship("Place", secondary=place_amenity)
