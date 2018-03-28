03ebd1faf28bd5f7918ea15efc84dd806f127021
KevinClauson <kevinclauson99@gmail.com>
2018-03-28 16:24:00 +0000

    add city place

--- models/city.py
+++ models/city.py
@@ -13,14 +13,9 @@ class City(BaseModel, Base):
 
     if (environ.get('HBNB_TYPE_STORAGE') == 'db'):
         __tablename__ = 'cities'
-        state_id = Column(String(60),
-                          ForeignKey('states.id'),
-                          nullable=False)
-        name = Column(String(128),
-                      nullable=False)
-        places = Column(relationship("Place",
-                                     backref='cities',
-                                     cascade='all, delete'))        
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", cascade='all, delete-orphan', backref='cities')
     else:
         state_id = ''                                                                                                                                                    
         name = ''
--- models/place.py
+++ models/place.py
@@ -7,22 +7,22 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Integer, Float
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     '''
         Define the class Place that inherits from BaseModel.
     '''
     if environ.get('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'places'
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
         name = Column(String(128), nullable=False)
-        description = Column(String(1024), nullable=True)
+        description = Column(String(1024))
         number_rooms = Column(Integer, nullable=False, default=0)
         number_bathrooms = Column(Integer, nullable=False, default=0)
         max_guest = Column(Integer, nullable=False, default=0)
         price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float, nullable=True, default=0)
-        longitude = Column(Float, nullable=True, default=0)
+        latitude = Column(Float)
+        longitude = Column(Float)
     else:
         city_id = ""
         user_id = ""
