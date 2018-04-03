8f31a2a07e5fc2fe0b097fb4333e37c78ac9d02a
KevinClauson <kevinclauson99@gmail.com>
2018-04-03 01:46:33 +0000

    edit line with line break

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,5 @@ mkdir -p /data/web_static/shared/
 echo "<p>Hello World<p>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/current /data/web_static/releases/test/
 chown -R ubuntu:ubuntu /data/
-sed -i "38i\ \tlocation \/hbnb_static {\\n\\t\\talias \/data\/web_static\/current\/;\\n\\t}" \ 
-    /etc/nginx/sites-available/default
+sed -i "38i\ \tlocation \/hbnb_static {\\n\\t\\talias \/data\/web_static\/current\/;\\n\\t}" /etc/nginx/sites-available/default
 service nginx restart
