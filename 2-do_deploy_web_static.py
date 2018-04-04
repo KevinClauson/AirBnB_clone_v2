04b9dd8c2ce0a48645ac3026c7302c56051d262c
KevinClauson <kevinclauson99@gmail.com>
2018-04-04 19:39:25 +0000

    change 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,30 +11,23 @@ env.hosts = ['54.175.3.213', '34.228.64.127']
 
 
 def do_deploy(archive_path):
-    ''' deploy to web servers 
-    '''
-    if not os.path.isfile(archive_path):
+    """puts tgx archive ont o webservers"""
+    if not os.path.exists(archive_path):
         return False
-    archive_name = archive_path.split("/")[1]
-    remote_name = '/tmp/' + archive_name
-    upload = put(archive_path, remote_name)
-    if upload.failed:
+    archive_name = archive_path.split('/')[-1]
+    archive_dir = archive_name.replace('.tgz', '')
+    remote_path = '/tmp/' + archive_name
+    if put(archive_path, remote_path).failed:
         return False
-    archive_dir = archive_name.split(".")[0]
     run('mkdir -p /data/web_static/releases/{}/'.format(archive_dir))
-
-
-    r = run('tar -xzf {} -C /data/web_static/releases/{}/'.format(remote_name, archive_dir))
-    if r.failed:
-        return False
-    r = run('rm {}'.format(remote_name))
-    if r.failed:
-        return False
-    run("cp -rp /data/web_static/releases/{}/web_static/*\
-        /data/web_static/releases/{}/".format(archive_name, archive_name))
-    run('rm -rf /data/web_static/releases/{}/web_static/'.format(archive_name))
-    r = run('rm -rf /data/web_static/current')
-    if r.failed:
-        return False
-    run('ln -sf /data/web_static/releases/{}/ /data/web_static/current'.format(archive_name))
+    run('tar -xzf {} -C /data/web_static/releases/{}/'.
+        format(remote_path, archive_dir))
+    run('rm {}'.format(remote_path))
+    run('mv /data/web_static/releases/{}/web_static/* '
+        '/data/web_static/releases/{}/'.
+        format(archive_dir, archive_dir))
+    run('rm -rf /data/web_static/releases/{}/web_static/'.format(archive_dir))
+    run('rm -rf /data/web_static/current')
+    run('ln -sf /data/web_static/releases/{}/ /data/web_static/current'.
+        format(archive_dir))
     return True
