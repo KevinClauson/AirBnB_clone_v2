c9a1e250246610c1fe5fae34a34cad38f27c87d3
KevinClauson <kevinclauson99@gmail.com>
2018-04-04 17:51:11 +0000

    change 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-# sets up your web servers: install Nginx creates folters
-apt-get update
-apt-get -y install nginx
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "<p>Hello World<p>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/current /data/web_static/releases/test/
-chown -R ubuntu:ubuntu /data/
-sed -i "38i\ \tlocation \/hbnb_static {\\n\\t\\talias \/data\/web_static\/current\/;\\n\\t}" /etc/nginx/sites-available/default
-service nginx restart
+# Creating server configuration
+sudo apt-get update
+sudo apt-get install nginx -y
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
+echo "Hello holberton" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i "38i\ \tlocation \/hbnb_static {\\n\\t\\talias \/data\/web_static\/current\/;\\n}" /etc/nginx/sites-available/default
+sudo service nginx restart
