cbb28ff789327175b4665a149fff6d24a34281bf
KevinClauson <kevinclauson99@gmail.com>
2018-04-19 05:50:09 +0000

    add 2 files

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+FLASK HELLO WORLD BASIC APP
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+port = 5000
+ip = '0.0.0.0'
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_fun(text):
+    r = text.replace('_', ' ')
+    return "C {}".format(r)
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python(text="is cool"):
+    r = text.replace('_', ' ')
+    return "Python {}".format(r)
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def num_template(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def odd_even(n):
+    if n % 2:
+        return render_template('6-number_odd_or_even.html', n=n, name='odd')
+    else:
+        return render_template('6-number_odd_or_even.html', n=n, name='even')
+
+
+if __name__ == "__main__":
+    """HELLO WORLD APP"""
+    app.run(host=ip, port=port)
