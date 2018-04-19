94a13c51d0c61e1890ddab3943f075706ddd4147
KevinClauson <kevinclauson99@gmail.com>
2018-04-19 04:18:26 +0000

    add file

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+FLASK HELLO WORLD BASIC APP
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+port = 5000
+ip = '0.0.0.0'
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_fun(text):
+    r = text.replace('_', ' ')
+    return "C {}".format(r)
+
+if __name__ == "__main__":
+    """HELLO WORLD APP"""
+    app.run(host=ip, port=port)
