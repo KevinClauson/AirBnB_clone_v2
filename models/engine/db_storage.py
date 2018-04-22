46438531287ef81b6102d940a136865f715d357c
KevinClauson <kevinclauson99@gmail.com>
2018-04-22 22:13:39 +0000

    add to db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -87,10 +87,10 @@ class DBStorage:
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine,
                                        expire_on_commit=False)
-        self.__session = Session()
+        self.__session = scoped_session(session_factory)
 
     def close(self):
         """
-           call remove() method on the private session attribute
+           reloads the current session
         """
         self.__session.remove()
