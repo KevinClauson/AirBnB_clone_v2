65aef8a2ae824aa88d1fdd9a30623a948ef44909
KevinClauson <kevinclauson99@gmail.com>
2018-04-23 04:04:58 +0000

    add 8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Flask app that renders states
+"""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+port = 5000
+ip = '0.0.0.0'
+
+
+@app.route('/cities_by_states')
+def states_list():
+    """ state list route """
+    state_objs = storage.all("State")
+    return render_template('8-cities_by_states.html', state_objs=state_objs)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    '''
+    After each request you must remove the current SQLAlchemy Session
+    '''
+    storage.close()
+
+
+if __name__ == "__main__":
+    """RUN APP"""
+    app.run(host=ip, port=port)
